60f4eb89459c9f01925ab4a435b1f01f73649ec0
Honoured10 <samuelogunmola4@gmail.com>
2022-12-16 10:32:53 +0100

    Fixed checker issue

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit)
+    last_digit = number % 10
+    if last_digit == 0 or number == 0:
+        return 0
+    elif (last_digit < 0) or (last_digit > 0):
+        return last_digit
